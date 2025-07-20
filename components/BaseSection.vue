6e418c3d86c73bb7a8297511b4dd8ed85c602f4c
brojor <bronislav.jordan@gmail.com>
2025-07-20 14:37:31 +0200

    style(components): adjust spacing in BaseSection component

--- components/BaseSection.vue
+++ components/BaseSection.vue
@@ -6,9 +6,9 @@ defineProps<{
 </script>
 
 <template>
-  <section :id="id" class="px-4 py-12 md:pt-16 md:pb-24 md:px-16 text-slate-900 even:bg-surface-even odd:bg-surface-odd">
+  <section :id="id" class="px-4 py-10 md:pt-16 md:pb-24 md:px-16 text-slate-900 even:bg-surface-even odd:bg-surface-odd">
     <div class="max-w-screen-xl mx-auto">
-      <h2 class="text-3xl font-semibold mb-12 font-heading">
+      <h2 class="text-3xl font-semibold mb-6 md:mb-12 font-heading">
         {{ title }}
       </h2>
       <div class="flex flex-col md:flex-row gap-16">
